1505bc93f541768ad7e177f39ecbd24170250830
YASH BOTHRA <yash.bothra@pramayo.co>
2026-03-03 09:43:19 +0530

    Update CORS origin to new production URL

--- Server/app.js
+++ Server/app.js
@@ -14,7 +14,7 @@ app.use(express.json());
 const PORT = process.env.PORT;
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://new-exam-notets-ai.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }),
